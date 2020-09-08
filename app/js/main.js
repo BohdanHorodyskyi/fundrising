0c45448014b00f68a421560d51bf986be042e479
BohdanHorodyskyi <bohdanhorodyskyi@gmail.com>
2020-09-08 15:38:14 +0300

    first commit'

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function() {
+    $(".vertical-center-4").slick({
+        dots: true,
+        vertical: true,
+        centerMode: true,
+        slidesToShow: 4,
+        slidesToScroll: 2
+      });
+    });
